1beb6a307416a170d05116c15134843a4d9e5bee
frstark <frstark@tsn.at>
2021-09-27 16:40:05 +0200

    Kommentar zu Ram hinzugefügt

--- src/Ram.java
+++ src/Ram.java
@@ -1,7 +1,16 @@
 public class Ram extends Komponente{
 
 
+    /**
+     *
+     * @param produktNummer
+     * @param hersteller
+     * @param modellBezeichnung
+     */
     public Ram(String produktNummer, String hersteller, String modellBezeichnung) {
         super(produktNummer, hersteller, modellBezeichnung);
+
+
+        //Kommentar
     }
 }
